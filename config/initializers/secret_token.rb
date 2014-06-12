53dfff90d426b65117386d29f28d4a2fbaba1d60
ayoolasolomon <ayoyink@yahoo.com>
2014-06-12 17:48:27 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RapCity::Application.config.secret_token = 'ee192616f9afa0e4ac4af31f0bbdd42224a64aef9a383aff569f7a7e54efac4a63243d64cb37801c44298a981c15026d5c8ebd71a094b7989dc60d7c2eee3f30'
